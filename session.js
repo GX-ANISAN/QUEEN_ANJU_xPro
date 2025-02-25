d8f918ef98b4358e47072658a8f50b89a16f655d
αηι - sαη <141144482+GX-ANISAN@users.noreply.github.com>
2025-02-25 12:38:45 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~3A9FxZaQ#efIxvY-4sEQkIL9kfjKfonn_GX1vo3RY3EkmCwFqhn0",
 PORT: process.env.PORT || "8000"
 };
